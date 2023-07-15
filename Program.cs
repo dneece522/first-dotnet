916f28183cea9598414e670b3c3964f4e9bd850a
Drew Neece <dneece5@aggienetwork.com>
2023-07-14 20:36:30 -0500

    first if else statement

--- Program.cs
+++ Program.cs
@@ -10,12 +10,36 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if (total > 14)
+if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+{
+  if (roll1 == roll2 && roll2 == roll3)
+  {
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+  }
+  else
+  {
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+  }
+}
+
+Console.WriteLine($"Total: {total}");
+
+if (total >= 15)
 {
   Console.WriteLine("You Win!");
 }
-
-if (total < 15)
+else
 {
   Console.WriteLine("Sorry, you lose.");
 }
+
+// string message = "The quick brown fox jumped over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if (message.Contains("fox"))
+// {
+//   Console.WriteLine("What does the fox say?");
+// }
