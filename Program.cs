61f8f072ab5536c0909274148884de3f1c060ee2
Drew Neece <dneece5@aggienetwork.com>
2023-07-14 20:53:01 -0500

    complete if-else tutorial

--- Program.cs
+++ Program.cs
@@ -1,45 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Random dice = new Random();
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// Your code goes here
 
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+if (daysUntilExpiration > 1)
 {
-  if (roll1 == roll2 && roll2 == roll3)
-  {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-  }
-  else
-  {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-  }
-}
-
-Console.WriteLine($"Total: {total}");
-
-if (total >= 15)
-{
-  Console.WriteLine("You Win!");
+  discountPercentage = 10;
 }
 else
 {
-  Console.WriteLine("Sorry, you lose.");
+  discountPercentage = 20;
 }
 
-// string message = "The quick brown fox jumped over the lazy dog.";
-// bool result = message.Contains("dog");
-// Console.WriteLine(result);
-
-// if (message.Contains("fox"))
-// {
-//   Console.WriteLine("What does the fox say?");
-// }
+if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+{
+  Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+{
+  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+}
+if (daysUntilExpiration == 1)
+{
+  Console.WriteLine($"Your subscription expires within a day.\nRenew now and save {discountPercentage}%!");
+}
+if (daysUntilExpiration == 0)
+{
+  Console.WriteLine($"Your subscription has expired.");
+}
